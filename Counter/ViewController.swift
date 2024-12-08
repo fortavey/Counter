608903caf4dce94054190449c2c35d4045c2991d
fortavey <fortavey@gmail.com>
2024-12-08 13:28:54 +0300

    test sprint_03 branch

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var cnt: Int = -1 {
+        didSet {
+            counterText.text = "Значение счётчика: \(cnt)"
+        }
+    }
 
+    @IBOutlet weak var counterText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        cnt = 0
     }
 
 
+    @IBAction func increaseButtonClick(_ sender: UIButton) {
+        cnt += 1
+    }
 }
 
